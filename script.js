1394a4832ad8a4c7e4364aeebadc46114f77b1dd
Yurii Daniuk <danukura2007@gmail.com>
2026-07-05 21:14:07 +0300

    Add modal functionality with open and close animations

--- script.js
+++ script.js
@@ -7,4 +7,18 @@ $(document).ready(function () {
 
     $("body, html").animate({scrollTop: positionFromTop}, 600);
   });
+
+  $(".btn-open-modal").click(function(){
+    $(".modal-overlay").fadeIn(300);
+  })
+
+  $(".btn-close-modal").click(function(){
+    $(".modal-overlay").fadeOut(300);
+  })
+
+  $(".modal-overlay").click(function(e){
+    if($(e.target).is(".modal-overlay")){
+        $(this).fadeOut(300);
+    }
+  })
 });
